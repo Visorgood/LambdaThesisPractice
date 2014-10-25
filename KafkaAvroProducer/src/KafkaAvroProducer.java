aca0898831c89219c5bb04f059076fc19129feb7
Viacheslav Inozemtsev <viacheslav.inozemtsev@gmail.com>
2014-10-25 16:18:17 +0200

    Update KafkaAvroProducer.java

--- KafkaAvroProducer/src/KafkaAvroProducer.java
+++ KafkaAvroProducer/src/KafkaAvroProducer.java
@@ -1,13 +1,3 @@
-/*
- * required libraries:
- * 	  kafka_2.9.2-0.8.1.1.jar
- * 	  log4j-1.2.15.jar
- * 	  metrics-core-2.2.0.jar
- * 	  scala-library-2.9.2.jar
- * 	  slf4j-api-1.7.2.jar
- *    
- */
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
@@ -39,29 +29,8 @@ public class KafkaAvroProducer
 		
 		ProducerConfig config = new ProducerConfig(props);
 		Producer<String, String> producer = new Producer<String, String>(config);
-		//sendRandomEvents(producer);
 		sendAvroEvents(producer);
 		producer.close();
-	}
-	
-	static void sendRandomEvents(Producer<String, String> producer) throws IOException
-	{
-		Random r = new Random();
-		for (String eventName : eventNames)
-		{
-			Schema schema = new Schema.Parser().parse(new File(eventName + ".avsc"));
-			GenericRecord record;
-			for (long i = 0; i < 10; ++i)
-			{
-				record = new GenericData.Record(schema);
-				record.put("id", r.nextLong() % 100000);
-				record.put("userId", r.nextLong() % 100000);
-				record.put("time", new Date().getTime());
-				//record.put("contactHash", "");
-				//record.put("msgLength", 0);
-				producer.send(new KeyedMessage<String, String>(eventName, record.toString()));
-			}
-		}
 	}
 	
 	static void sendAvroEvents(Producer<String, String> producer) throws IOException
